dd10f2263de7cf9ee38da86149bc14b16f745b69
Liam Sharp <liam.sharp@screamingfrog.co.uk>
2020-09-30 13:13:50 +0100

    Bit more logging, clean up build warnings

--- src/main/java/com/github/liamsharp/App.java
+++ src/main/java/com/github/liamsharp/App.java
@@ -7,9 +7,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Logging fonts:");
+        System.out.println("Getting local graphics environment");
+        final GraphicsEnvironment localGraphicsEnvironment = 
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
         
-        final Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        System.out.println("Getting all fonts");
+        final Font[] fonts = localGraphicsEnvironment.getAllFonts();
+        System.out.println("Logging " + fonts.length + " fonts");
         for (Font font : fonts)
         {
             System.out.println(font);
