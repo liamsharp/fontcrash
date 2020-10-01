32ab264b63d0e8bdcc95514e38fa185b8356c7f0
Liam Sharp <liam.sharp@screamingfrog.co.uk>
2020-10-01 11:12:16 +0100

    Try some more stuff

--- src/main/java/com/github/liamsharp/App.java
+++ src/main/java/com/github/liamsharp/App.java
@@ -1,12 +1,28 @@
 package com.github.liamsharp;
 
+import java.awt.Canvas;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
+
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class App 
 {
     public static void main( String[] args )
     {
+        System.out.println("Setting Windows look and feel");
+        
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (IllegalAccessException | ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException e)
+        {
+            System.err.println("Unable to set system look and feel: " + e);
+        }
+        
         System.out.println("Getting local graphics environment");
         final GraphicsEnvironment localGraphicsEnvironment = 
                 GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -19,6 +35,13 @@ public class App
             System.out.println(font);
         }
         System.out.println("done");
+
+        System.out.println("Creating JLabel");
+        JLabel jLabel  = new JLabel();
+        System.out.println("done " + jLabel);
         
+        System.out.println("Creating Canvas");
+        Canvas canvas = new Canvas();
+        System.out.println("done" + canvas);
     }
 }
